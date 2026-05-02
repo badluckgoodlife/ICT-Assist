8553752804fceecc0ade75fe3008de408040a9ca
Baker <baker@yonsei.ac.kr>
2026-05-02 12:24:05 +0900

    Update run.py

--- run.py
+++ run.py
@@ -4,9 +4,16 @@ import subprocess
 import sys
 import os
 
-###env = os.environ.copy()
-###env["FORCE_COLOR"] = "1"
-
-###subprocess.run([sys.executable, "main.py", "--symbol", "ETHUSDT", "--tf", "1H-15m", "--watch", "--interval", "300"],env=env)
-
+'''
+subprocess.run([sys.executable, "pip", "install", "-r", "requirements.txt"])
+'''
+'''
+---Direct Trigger---
+env = os.environ.copy()
+env["FORCE_COLOR"] = "1"
+subprocess.run([sys.executable, "main.py", "--symbol", "ETHUSDT", "--tf", "1H-15m", "--watch", "--interval", "300"],env=env)
+'''
+'''
+###---Bot start---
 subprocess.run([sys.executable, "bot.py"])
+'''
